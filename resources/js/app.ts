ef7b7317045fdee44945dc7a577a3f6f211facdc
Traykovski <traykovski5@gmail.com>
2024-02-22 00:12:48 +0100

    remove comments

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -3,25 +3,6 @@ import '@types/@inertiajs__svelte'
 
 import { createInertiaApp } from '@inertiajs/svelte';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import router from 'ziggy-js';
-
-// declare global {
-//     interface Window {
-//         route: typeof router
-//     }
-// }
-
-
-declare let window: Window & typeof globalThis & {
-    route: typeof router
-};
-// window.route = window.route;
-
-// window.route()
-// window.route()
-
-
-// window.route()
 
 createInertiaApp({
     resolve: (name) => resolvePageComponent(`./Pages/${name}.svelte`, import.meta.glob('./Pages/**/*.svelte')),
