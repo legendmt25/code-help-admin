247234ded3ccc0a3186d5e5cad0066c1efd1c6ef
Трајковски Мартин <martintrajkovski70@gmail.com>
2024-04-27 02:19:53 +0200

    - oauth2 token verification auth middleware update

--- code-help-admin-backend/admin-api/go.mod
+++ code-help-admin-backend/admin-api/go.mod
@@ -10,10 +10,17 @@ require (
 )
 
 require (
+	github.com/coreos/go-oidc/v3 v3.10.0 // indirect
+	github.com/go-jose/go-jose/v4 v4.0.1 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/google/uuid v1.5.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
-	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/crypto v0.19.0 // indirect
+	golang.org/x/oauth2 v0.13.0 // indirect
+	golang.org/x/sys v0.17.0 // indirect
+	google.golang.org/appengine v1.6.8 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
 )
--- code-help-admin-backend/admin-api/internal/api/auth_midleware.go
+++ code-help-admin-backend/admin-api/internal/api/auth_midleware.go
@@ -2,7 +2,12 @@ package api
 
 import (
 	"context"
+	"fmt"
 	"net/http"
+	"os"
+	"strings"
+
+	"github.com/coreos/go-oidc/v3/oidc"
 )
 
 type AuthMiddleware struct {
@@ -19,5 +24,32 @@ type AuthCtx struct {
 
 func (a AuthMiddleware) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
 	ctx := context.WithValue(request.Context(), "", AuthCtx{})
+
+	authorizationHeaderValues := request.Header["Authorization"]
+
+	if len(authorizationHeaderValues) == 0 || authorizationHeaderValues[0] == "" {
+		a.handler.ServeHTTP(writer, request.WithContext(ctx))
+		return
+	}
+
+	idToken := strings.Replace(authorizationHeaderValues[0], "Bearer ", "", 1)
+
+	issuerUri := os.Getenv("OAUTH2_RESOURCESERVER_JWT_ISSUERURI")
+	jwtSetUri := os.Getenv("OAUTH2_RESOURCESERVER_JWT_JWKSETURI")
+
+	remoteKeySet := oidc.NewRemoteKeySet(ctx, jwtSetUri)
+
+	oidcConfig := oidc.Config{SkipClientIDCheck: true}
+
+	verifier := oidc.NewVerifier(issuerUri, remoteKeySet, &oidcConfig)
+	token, err := verifier.Verify(ctx, idToken)
+
+	if err != nil {
+		fmt.Printf("oauth2: error when fetching token: %s,%s\n", err.Error(), token)
+		a.handler.ServeHTTP(writer, request.WithContext(ctx))
+	} else {
+		fmt.Print("\n Token verification success! \n")
+	}
+
 	a.handler.ServeHTTP(writer, request.WithContext(ctx))
 }
